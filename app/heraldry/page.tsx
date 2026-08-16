4d161a0ff253a21cbc710b751149b0bcb2e545d8
kramey770 <33795000+kramey770@users.noreply.github.com>
2026-08-16 21:31:48 +0000

    Integrate Armoria heraldry creator

--- app/heraldry/page.tsx
+++ app/heraldry/page.tsx
@@ -2,7 +2,7 @@ export default function HeraldryPage() {
   return (
     <div className="h-screen w-full">
       <iframe
-        src="https://scaling-palm-tree-g5xrp6v9wxpc9vwg-5000.app.github.dev"
+        src="/armoria/index.html"
         title="Armoria Heraldry Creator"
         className="h-full w-full border-0"
       />
